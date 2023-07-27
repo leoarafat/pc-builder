d5aec6ae88fe3aabb7f8e72a55ab1916dadb5652
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-07-27 22:18:03 +0800

    Project initialize

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return (
+    <SessionProvider session={pageProps.session}>
+      {getLayout(<Component {...pageProps} />)}
+    </SessionProvider>
+  );
 }
